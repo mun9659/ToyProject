13906ad5c24b900d9b580c7a94239c6b159544c9
Mun Kyoung-min <mun9659@naver.com>
2023-01-14 15:42:27 +0900

    환경세팅 완료

---
+++ src/main/java/com/toyProject/config/MvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.toyProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+					.addResourceLocations("classpath:/templates/", "classpath:/static/");
+	}
+	
+}
---
+++ src/test/java/com/toyProject/ToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
