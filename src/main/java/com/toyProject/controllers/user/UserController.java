65bf1c2e007d28a29c88efe08ccf48e243be0a21
Mun Kyoung-min <mun9659@naver.com>
2023-01-17 22:00:34 +0900

    [Rename] JPA SELECT, UPDATE 추가

--- src/main/java/com/toyProject/controllers/user/UserController.java
+++ src/main/java/com/toyProject/controllers/user/UserController.java
@@ -1,12 +1,24 @@
 package com.toyProject.controllers.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.toyProject.models.entity.user.RoleType;
 import com.toyProject.models.entity.user.User;
 import com.toyProject.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
 
 @RestController
 public class UserController {
@@ -28,5 +40,42 @@ public class UserController {
 		userRepository.save(user); // Insert 작업
 		
 		return "회원가입이 완료되었습니다.";
+	}
+	
+	@GetMapping("/user/{id}")
+	public User findUser(@PathVariable Long id) {
+		
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			// IllegalArgumentException 예외 처리
+			throw new IllegalArgumentException("해당하는 아이디가 없습니다 id : " + id);
+		});
+		
+		return user;
+	}
+	
+	@GetMapping("/user")
+	public List<User> findAllUser() {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/user/page/{page}")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable) {
+		Page<User> users = userRepository.findAll(pageable);
+		List<User> list = users.getContent();
+		return list;
+	}
+	
+	@Transactional
+	@PutMapping("/user/{id}")
+	public void update(@PathVariable Long id, @RequestBody User requestUser) {
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			// IllegalArgumentException 예외 처리
+			throw new IllegalArgumentException("해당하는 아이디가 없습니다 id : " + id);
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		// userRepository.save(user);
 	}
 }
