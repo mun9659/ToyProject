072dcf83bd378d04a6db890937a1c885f7152440
Mun Kyoung-min <mun9659@naver.com>
2023-01-21 15:46:42 +0900

    [Feat] 로그인, 회원가입 페이지 생성

--- src/main/java/com/toyProject/controller/user/UserController.java
+++ src/main/java/com/toyProject/controller/user/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,74 +23,19 @@ import com.toyProject.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
-@RestController
+@Controller
 public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/user/join")
-	public String join(User user) {
-		System.out.println("id : " + user.getId());
-		System.out.println("username : " + user.getUsername());
-		System.out.println("password : " + user.getPassword());
-		System.out.println("email : " + user.getEmail());
-		System.out.println("role : " + user.getRole());
-		System.out.println("regDt : " + user.getRegDt());
-		System.out.println("modDt : " + user.getModDt());
-		
-		user.setRole(RoleType.USER);
-		userRepository.save(user); // Insert 작업
-		
-		return "회원가입이 완료되었습니다.";
+	@GetMapping("/user/joinForm")
+	public String join() {
+		return "user/joinForm";
 	}
 	
-	@GetMapping("/user/{id}")
-	public User findUser(@PathVariable Long id) {
-		
-		User user = userRepository.findById(id).orElseThrow(() -> {
-			// IllegalArgumentException 예외 처리
-			throw new IllegalArgumentException("해당하는 아이디가 없습니다 id : " + id);
-		});
-		
-		return user;
-	}
-	
-	@GetMapping("/user")
-	public List<User> findAllUser() {
-		return userRepository.findAll();
-	}
-	
-	@GetMapping("/user/page/{page}")
-	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable) {
-		Page<User> users = userRepository.findAll(pageable);
-		List<User> list = users.getContent();
-		return list;
-	}
-	
-	@Transactional
-	@PutMapping("/user/{id}")
-	public void update(@PathVariable Long id, @RequestBody User requestUser) {
-		User user = userRepository.findById(id).orElseThrow(() -> {
-			// IllegalArgumentException 예외 처리
-			throw new IllegalArgumentException("해당하는 아이디가 없습니다 id : " + id);
-		});
-		
-		user.setPassword(requestUser.getPassword());
-		user.setEmail(requestUser.getEmail());
-		
-		// userRepository.save(user);
-	}
-	
-	@DeleteMapping("/user/{id}")
-	public String delete(@PathVariable Long id) {
-		
-		try {
-			userRepository.deleteById(id);
-		} catch (EmptyResultDataAccessException e) {
-			return "삭제에 실패하였습니다. 해당 id는 DB에 존재하지 않습니다.";
-		}
-		
-		return "삭제가 완료되었습니다. id : " + id;
+	@GetMapping("/user/loginForm")
+	public String login() {
+		return "user/loginForm";
 	}
 }
